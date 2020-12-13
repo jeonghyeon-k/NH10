5f272864ff6ec98ba939be4bc8dfcbe38a2a0dd8
jeonghyeon kim <rlawjd147@gmail.com>
2020-12-13 16:21:19 +0900

    - edit api

--- Activity.js
+++ Activity.js
@@ -1,20 +1,32 @@
 const get = require("./api.js")
 
-let Acitivity = ""
+const gettimestamp = () => {
+    let settime = new Date().getUTCMilliseconds()
+    if (settime < 100) {
+        settime = "00" + settime
+    } else if (99 < settime && settime < 1000) {
+        settime = "0" + settime
+    } else {
+        gettimestamp()
+    }
+    return settime
+}
+
 //예금주 조회
 exports.accout = async (param) => {
-    param.Header = ""
-    console.log(param)
+    let timestamp = gettimestamp()
     return await get.api({
         apiNm: "InquireDepositorAccountNumber",
         url: `InquireDepositorAccountNumber.nh`,
         type: "post",
         param,
+        isTuno: timestamp,
     })
 }
 
 //거래내역조회
 exports.transaction = async (param) => {
+    let timestamp = gettimestamp()
     return await get.api({
         apiNm: "InquireTransactionHistory",
         url: `InquireTransactionHistory.nh`,
@@ -25,6 +37,7 @@ exports.transaction = async (param) => {
 
 //잔액조회
 exports.balance = async (param) => {
+    let timestamp = gettimestamp()
     return await get.api({
         apiNm: "InquireBalance",
         url: `InquireBalance.nh`,
@@ -35,6 +48,7 @@ exports.balance = async (param) => {
 
 //개인카드 승인내역 조회
 exports.card = async (param) => {
+    let timestamp = gettimestamp()
     return await get.api({
         apiNm: "InquireCreditCardAuthorizationHistory",
         url: `InquireCreditCardAuthorizationHistory.nh`,
--- api.js
+++ api.js
@@ -13,24 +13,12 @@ if (("" + day).length == 1) {
     day = "0" + day
 }
 
-const gettimestamp = () => {
-    let settime = new Date().getUTCMilliseconds()
-    if (settime < 100) {
-        settime = "00" + settime
-    } else if (99 < settime && settime < 1000) {
-        settime = "0" + settime
-    } else {
-        gettimestamp()
-    }
-    return settime
-}
-const timestamp = gettimestamp()
 const tsymd = year + "" + month + "" + day
 const iscd = "000530" //기관코드
 const accessToKen =
     "74608b853a7178ab3b779ea5a44c2a3826dd8af687e4811d6362e934c5983ad0" //인증키
 
-exports.api = ({ url, type, param, apiNm }) => {
+exports.api = ({ url, type, param, apiNm, isTuno }) => {
     param.Header = {
         ApiNm: apiNm,
         Tsymd: tsymd,
@@ -38,7 +26,7 @@ exports.api = ({ url, type, param, apiNm }) => {
         Iscd: iscd,
         FintechApsno: "001",
         ApiSvcCd: "DrawingTransferA",
-        IsTuno: timestamp, // 카운트로 변경 해야함
+        IsTuno: isTuno, // 카운트로 변경 해야함
         AccessToken: accessToKen,
     }
 
